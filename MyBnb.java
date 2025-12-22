fcb7c69a96c52b6b1210bf2ad5c3d062b54b9e88
jairius519 <155593713+jairius519@users.noreply.github.com>
2025-12-21 23:33:31 -0600

    Add files via upload

---
+++ MyBnb.java
@@ -0,0 +1,118 @@
+import java.io.*;
+import java.util.*;
+
+/*
+    Date: October 4, 2023
+    Program Description: MyBnb is used for renting and has various rooms followed by staff.
+    Name: Jairius Burks
+ */
+
+ /**
+    * MyBnb is ued for renting and has various rooms followed by staff.
+    * Anyone can chose their own unique room
+*/
+public class MyBnb { 
+
+   /**
+      * A list of rental room objects
+   */
+   List<RentalRoom> elements = new ArrayList<>();
+   
+   /**
+      * Mybnb constructor that reads from a file and stores that data into a arrayList
+      * @param fileName file name of the room id and beds 
+      */
+   public MyBnb(String fileName) {
+   
+      try {
+         Scanner keyboard = new Scanner(new File(fileName));
+        
+         while(keyboard.hasNext()) {
+            String information = keyboard.next();
+            int bedNumbers = keyboard.nextInt();
+            elements.add(new RentalRoom(information, bedNumbers));
+               
+         }
+           
+      }
+      
+      catch(FileNotFoundException e ) {
+         System.out.println("File Not Found");
+      }
+        
+                  
+   }
+    
+    /**
+      * Loops through all of the rooms and if the number of beds equals the beds, accumulate.
+      * @param beds beds in the Bnb
+      * @return the count 
+     */
+   public int numberOfRooms(int beds) {
+      int  count = 0;
+      
+      for(int i = 0; i < elements.size(); i++) {
+         if(elements.get(i).getNumBeds()== beds)
+            count++;
+             
+      }
+   
+      return count;
+   }
+       
+    /**
+      * Loops through the rooms and chooses the first room that's available with
+      * at least the minnimum number of beds.
+      * @param minBeds The minimum number of beds
+      * @return the room ID. returns NONE if no room is available 
+       
+     */
+   public String chooseRoom(int minBeds) {
+   
+      boolean choice = false;
+    
+      for(int index = 0; index < elements.size(); index++) {
+         RentalRoom room = elements.get(index);
+      
+         if(room.getNumBeds() >= minBeds && room.isAvailable())  {
+            room.setAvailable(choice);
+            return room.getID();
+         }
+      
+            
+      }
+      return "NONE";
+   }
+
+    /**
+     * returns the number of staff needed to clean the rooms that are not available
+     * two staff members are needed to clean each room with three or more beds
+     * one staff member is needed for each group of three one-bed rooms
+     * @return the number of staff needed to clean the rooms that are occupied
+     */
+   public int numberOfStaffNeeded() {
+   
+      int threeBedRooms = 0;
+      int oneBedRooms = 0;
+      int twoBedRooms = 0;
+      
+      for(RentalRoom rooms : elements) {
+         if(rooms.getNumBeds() >= 3)
+            threeBedRooms++;
+         
+         else if(rooms.getNumBeds() == 1)
+            oneBedRooms++;
+         
+         else if(rooms.getNumBeds()== 2)
+            twoBedRooms++;
+      }
+   
+      int staff = 1 * threeBedRooms + (int)Math.ceil((oneBedRooms + 2  * twoBedRooms ) /3) ;
+      return staff;
+   
+        
+   
+   }
+    
+}  
+
---
+++ RoomsTest.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+   Program to test the basic functionality of the MyBnb class.
+*/
+public class RoomsTest
+{
+   public static void main(String [] args) throws FileNotFoundException
+   {
+      MyBnb rentals = new MyBnb("rental1.txt");
+      Scanner scan = new Scanner(new File("rental1.txt"));
+      while (scan.hasNextLine()) {
+         String elements = scan.nextLine();
+         System.out.println(elements);
+      }
+
+        
+      System.out.printf("Number of rooms with 2 bed(s): %d\n", 
+                                      rentals.numberOfRooms(2));    // Expected: 3
+    
+      System.out.println(rentals.chooseRoom(3));                    // Expected: AB1213
+      System.out.println(rentals.numberOfStaffNeeded());            // Expected: 2
+      System.out.println(rentals.chooseRoom(1));                    // Expected: AB1212
+      System.out.println(rentals.numberOfStaffNeeded());            // Expected: 3
+      System.out.println(rentals.chooseRoom(1));                    // Expected: AB233
+      System.out.println(rentals.numberOfStaffNeeded());            // Expected: 3
+       
+      
+   }
+}
+
